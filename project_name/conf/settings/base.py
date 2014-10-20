7cda6f4e177879aea66f4aa2d5d5d864fb2f8bcb
Rodrigo Guzman <rodguze@gmail.com>
2014-10-20 17:21:43 -0400

    configure logging in base settings. closes #1

--- project_name/conf/settings/base.py
+++ project_name/conf/settings/base.py
@@ -202,16 +202,20 @@ INSTALLED_APPS = DJANGO_APPS + LOCAL_APPS
 ########## END APP CONFIGURATION
 
 
-########## LOGGING CONFIGURATION
-# See: https://docs.djangoproject.com/en/dev/ref/settings/#logging
-# A sample logging configuration. The only tangible logging
-# performed by this configuration is to send an email to
-# the site admins on every HTTP 500 error when DEBUG=False.
-# See http://docs.djangoproject.com/en/dev/topics/logging for
-# more details on how to customize your logging configuration.
 LOGGING = {
     'version': 1,
     'disable_existing_loggers': False,
+    'formatters': {
+        'verbose': {
+            'format': ('%(asctime)s [%(process)d] [%(levelname)s] ' +
+                       'pathname=%(pathname)s lineno=%(lineno)s ' +
+                       'funcname=%(funcName)s %(message)s'),
+            'datefmt': '%Y-%m-%d %H:%M:%S'
+        },
+        'simple': {
+            'format': '%(levelname)s %(message)s'
+        }
+    },
     'filters': {
         'require_debug_false': {
             '()': 'django.utils.log.RequireDebugFalse'
@@ -222,17 +226,39 @@ LOGGING = {
             'level': 'ERROR',
             'filters': ['require_debug_false'],
             'class': 'django.utils.log.AdminEmailHandler'
+        },
+        'console': {
+            'level': 'DEBUG',
+            'class': 'logging.StreamHandler',
+            'formatter': 'verbose'
         }
     },
     'loggers': {
+        'django': {
+            'handlers':['console'],
+            'propagate': False,
+            'level':'INFO',
+        },
         'django.request': {
             'handlers': ['mail_admins'],
             'level': 'ERROR',
             'propagate': True,
         },
+        'django.db.backends': {
+            'handlers': ['console'],
+            'level': 'INFO',
+            'propogate': False,
+        },
+        '': {
+            'handlers':['console'],
+            'propagate': False,
+            'level':'INFO',
+        },
+
     }
 }
 ########## END LOGGING CONFIGURATION
+
 
 
 ########## WSGI CONFIGURATION
--- project_name/core/views.py
+++ project_name/core/views.py
@@ -1,3 +1,5 @@
+import logging
+
 from django.shortcuts import render, get_object_or_404
 from django.conf import settings
 from django.http import Http404, HttpResponse, JsonResponse
