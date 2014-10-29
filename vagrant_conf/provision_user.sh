de79463622dfc04e05c88c559757c4f7214a4f07
Leif Singer <git@singer.sh>
2014-10-29 15:36:54 +0100

    add heroku-config plugin to vagrant config

--- vagrant_conf/provision_user.sh
+++ vagrant_conf/provision_user.sh
@@ -31,7 +31,10 @@ install_heroku_toolbelt() {
     # without these using foreman to run the webserver throws an error, seems like a bug on their side:
     sudo apt-get install -y ruby-rvm
     sudo gem install foreman
+    # e.g. for re-deploying without pushing
     heroku plugins:install https://github.com/heroku/heroku-repo.git
+    # for reading and writing heroku environment variables to / from an env file
+    heroku plugins:install https://github.com/ddollar/heroku-config.git
 }
 
 init_db() {
